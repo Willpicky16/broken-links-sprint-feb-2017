8d01ba6bedfaeca86f78288f081f9a3a96ce45f8
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-12 12:30:58 +0100

    created functions to validate url and write url to file

---
+++ main.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function writeUrlToFile(url) {
+    fs.writeFile("initialUrl.json", url, "utf8", (err) => {
+        if (err) throw err;
+        console.log('file has been saved');
+    });
+}
+
+function validateUrl(url) {
+    var re = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+    if (!re.test(url)) {
+        console.log('url error');
+        return false;
+    } else {
+        console.log('success');
+    }
+}
+
+function pingUrl(url) {
+
+}
+
+writeUrlToFile('http://www.msn.com');
