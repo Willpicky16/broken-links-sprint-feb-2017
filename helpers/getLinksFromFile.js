7b4589b11344c4fd02418a5be89fa66a767e4326
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-13 15:33:05 +0100

    [feat-10] regex function working

--- helpers/getLinksFromFile.js
+++ helpers/getLinksFromFile.js
@@ -1,16 +1,29 @@
 const fs = require('fs');
 
-function getLinksFromFile(text) {
-    let initialStr = (fs.readFileSync(text).toString().match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g));
-    // let initialStr = text.toString().match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g);
-
+function getLinksFromFile() {
+    let initialStr = (fs.readFileSync('test.txt').toString().match(/href="(.*?)"/g));
+    
     let res = [];
     for (let i = 0; i < initialStr.length; i++) {
-        res.push(initialStr[i].slice(9, -1));
+        res.push(initialStr[i].slice(6, -1));
     }
-    return res;
+
+    let links = [];
+    for (let i = 0; i < res.length; i++) {
+        console.log(res[i].charAt(0));
+        if (res[i].charAt(0) === '/') {
+            links.push(res[i]);
+        }
+    }
+
+    fs.writeFile('link-array.txt', JSON.stringify(links), function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+    });
 }
 
+getLinksFromFile();
+
 if (typeof module !== 'undefined') {
-  module.exports = getLinksFromFile;
+    module.exports = getLinksFromFile;
 }
